a09012b92d359ab9c370a13ab5a71313f2fc3653
Danilo Vorkapic <danilo.vorkapic@ssoe.rs>
2023-08-27 21:36:34 +0200

    Create app with login, register and home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import TopNavbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Page">
+      <BrowserRouter className="App">
+        <div className="NavBar">
+          <TopNavbar />
+        </div>
+        <div className="Main">
+          <div className="CenterComponent">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
